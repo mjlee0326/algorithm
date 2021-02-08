4ec49ca7dc94d903000c0576058762c1a0fd5bac
mjlee0326 <mjlee032667@gmail.com>
2021-02-08 13:13:51 +0900

    다익스트라

--- 1753.c
+++ 1753.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-int findMinLength(){
-	
-}
-
 int main(){
 	int numE; //edge 총 개수
 	int numV; //vertex 총 개수
@@ -13,24 +9,25 @@ int main(){
 	scanf("%d", &numE);
 	scanf("%d", &startV);
 
-	int touch[numV+1];
+	//문제는 1부터 시작하는데, 배열 0부터 쓰기 헷갈려서 [0]은 그냥 쓰레기값으로 냅둠
 	int length[numV+1];
 	int weight[numV+1][numV+1];
 
-	int sol[numV];
-	sol[0]=0; //start value
-
 	int a, b, c;
-	int min=99; //given weight max=10;
+	int min; 
+	int vnear=startV; //
 
 	//initial weight array
 	for(int i=1; i<=numV; i++){
-		for(int j=1; j=<numV; j++){
-			weight[i][j]=-1;
+		for(int j=1; j<=numV; j++){
+			weight[i][j]=99;
+			if(i==j){
+				weight[i][j]=0;
+			}
 		}
 	}
 
-	//set weight arr value
+	//set weight value
 	for(int i=0; i<numE; i++){
 		scanf("%d", &a);
 		scanf("%d", &b);
@@ -39,27 +36,37 @@ int main(){
 		weight[a][b]=c;
 	}
 
-	for(int i=1; i<=numV; i++){
-		touch[i]=1; //현재 최단 경로의 마지막 마디
-		length[i]=weight[1][i]; //현재 확정된 마디에서, 마디간의 최소 길이
-	}
-
 
 	for(int i=1; i<=numV; i++){
-		if(0<length[i]<min){
-			min=length[i];
-			//나머지는 집에서
-		}
+		length[i]=weight[startV][i]; //현재 확정된 마디에서, 마디간의 최소 길이
 	}
+	
 
-	/*
-	Debug
-	printf("numE: %d, numV: %d startV: %d\n", numE, numV, startV);
-	for(int i=0; i<numV+1; i++){
-		for(int j=0; j<numV+1; j++){
-			printf("weight[%d][%d]=%d\n", i, j, weight[i][j]);
+	for(int k=1; k<=numV; k++){
+		min=99; //given weight max=10;
+		vnear=-1;
+
+		for(int i=1; i<=numV; i++){
+			if(0<=length[i] && length[i]<min){
+				min=length[i];
+				vnear=i; //최단 경로를 제공해주는 vertex index 저장
+			}
 		}
-	}
-*/
 
+		if(vnear!=-1){
+			printf("%d\n", length[vnear]);
+		}
+		else{
+			printf("INF\n");
+			return 0;
+		}
+
+		//length 재설정
+		for(int i=1; i<=numV; i++){
+			if(length[i] > length[vnear]+weight[vnear][i]){
+				length[i]=length[vnear]+weight[vnear][i];
+			}
+		}
+		length[vnear]=-1;
+	}
 }

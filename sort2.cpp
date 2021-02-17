c2d5c3bd7910f26769889f7df3a263a3558d0dc9
mjlee0326 <mjlee032667@gmail.com>
2021-02-18 03:32:16 +0900

    ^

--- sort2.cpp
+++ sort2.cpp
@@ -12,6 +12,7 @@ string solution(vector<int> numbers) {
 		vector<string> strVec;
 		vector<int> copy(numbers);
 		int large, index, num;
+		int j;
 		int size;
 		string str1, str2;
 		string str;
@@ -22,12 +23,14 @@ string solution(vector<int> numbers) {
 		}
 
 		sort(strVec.begin(), strVec.end(), greater<>());
-		
+
 		for(int i=0; i<strVec.size(); i++){
 			cout<<strVec[i]<<"  ";
 		}
-
+		
 		cout<<endl;
+
+
 
 		if(strVec[0]=="0"){ //all zero
 			answer+=strVec[0];
@@ -50,14 +53,23 @@ string solution(vector<int> numbers) {
 			}
 			if(strVec.size()>1 && strVec[0][0]==strVec[1][0]){ //앞자리가 같음
 				index=-1;
-				for(int i=1; i<MAX_DIGIT; i++){
+				for(int i=1; ; i++){
+					/*
 					if(strVec[0][i]=='\0' && strVec[1][i]=='\0'){
 						index=0;
 						break;
 					}
+					*/
 					if(strVec[0][i]<strVec[1][i]){ //strVec[0][i]가 null일 가능성 
 						if(strVec[0][i]=='\0'){
-							if(strVec[0][0]>strVec[1][i]){
+							while(strVec[1][i+1]!='\0'){ //strVec[1]의 끝자리
+								i++;
+							}
+							j=0;
+							while(strVec[0][j]==strVec[1][i]){
+								j++;
+							}
+							if(strVec[0][j]>strVec[1][i]){
 								index=0;
 								break;
 							}
@@ -67,7 +79,14 @@ string solution(vector<int> numbers) {
 					}
 					else if(strVec[0][i]>strVec[1][i]){ //strVec[1][i]가 null일 가능성
 						if(strVec[1][i]=='\0'){
-							if(strVec[1][0]>strVec[0][i]){
+							while(strVec[0][i+1]!='\0'){ //strVec[0]의 끝자리
+								i++;
+							}
+							j=0;
+							while(strVec[1][j]==strVec[0][i]){
+								j++;
+							}
+							if(strVec[1][j]>strVec[0][i]){
 								index=1;
 								break;
 							}
@@ -94,10 +113,18 @@ int main(){
 	vector<int> numbers5={1000, 0, 5, 99, 100};
 	vector<int> numbers6={403, 40};
 	vector<int> numbers7={21, 212};
-	vector<int> numbers8={10, 101};
-	vector<int> numbers9={1, 11, 111, 1111};
-	vector<int> numbers10={0, 5, 10, 15, 20};
-	vector<int> numbers11={33, 33};
+	vector<int> numbers8={212, 21};
+	vector<int> numbers9={10, 101};
+	vector<int> numbers10={1, 11, 111, 1111};
+	vector<int> numbers11={0, 5, 10, 15, 20};
+	vector<int> numbers12={33, 33};
+	vector<int> numbers13={12, 121};
+	vector<int> numbers14={121, 12};
+	vector<int> numbers15={0, 1000, 0, 0};
+	vector<int> numbers16={33, 9, 33, 25, 21, 212, 8, 0, 21};
+	vector<int> numbers17={40, 403};
+	vector<int> numbers18={998, 9, 999};
+	vector<int> numbers19={9, 99991, 91};
 	string answer;
 
 	answer=solution(numbers);
@@ -122,6 +149,21 @@ int main(){
 	cout<<answer<<endl;
 	answer=solution(numbers11);
 	cout<<answer<<endl;
-
+	answer=solution(numbers12);
+	cout<<answer<<endl;
+	answer=solution(numbers13);
+	cout<<answer<<endl;
+	answer=solution(numbers14);
+	cout<<answer<<endl;
+	answer=solution(numbers15);
+	cout<<answer<<endl;
+	answer=solution(numbers16);
+	cout<<answer<<endl;
+	answer=solution(numbers17);
+	cout<<answer<<endl;
+	answer=solution(numbers18);
+	cout<<answer<<endl;
+	answer=solution(numbers19);
+	cout<<answer<<endl;
 	return 0;
 }

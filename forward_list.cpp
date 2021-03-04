6d6b24d59209a6aba7d7d2ba6255bc4411b0f1f7
mjlee0326 <mjlee032667@gmail.com>
2021-03-05 00:14:11 +0900

    forward_list.cpp

--- forward_list.cpp
+++ forward_list.cpp
@@ -13,6 +13,7 @@ ostream &operator<<(ostream &ostr, const citizen &c){
 	return(ostr<<"["<<c.name<<", "<<c.age<<"]");
 }
 
+
 int main(){
 	forward_list<citizen> citizens={
 		{"Kim", 20}, {"Lee", 25}, {"Park", 33}, {"Jin", 42}
@@ -20,9 +21,32 @@ int main(){
 
 	auto citizens_copy=citizens; //deep copy
 
-	cout<<"전체 시민들: ";
+	cout<<"All citizens: ";
 	for(const auto &c :citizens){
 		cout<<c<<" ";
 	}
 	cout<<endl;
+
+	//lambda expression
+	citizens.remove_if([](const citizen &c){
+				return (c.age<26);
+			});
+				
+	cout<<"older than 26: ";
+	for(const auto &c :citizens){
+		cout<<c<<" ";
+	}
+	cout<<endl;
+
+	citizens_copy.remove_if([](const citizen &c){
+				return (c.age!=25);
+			});
+
+	cout<<"age 25: ";
+	for(const auto &c :citizens_copy){
+		cout<<c<<" ";
+	}
+	cout<<endl;
+
+
 }

64a7b99d390c7d24378ffefdc59a554f4f5b7a74
mjlee0326 <mjlee032667@gmail.com>
2021-04-02 01:52:20 +0900

    그래프_인접리스트

---
+++ graph_list.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+enum class city : int{
+	MOSCOW,
+	LONDON,
+	SEOUL,
+	SEATTLE,
+	DUBAI,
+	SYDNEY
+};
+
+ostream& operator<<(ostream& os, const city c){
+	switch(c){
+		case city::MOSCOW:
+			os<<"모스크바";
+			return os;
+		case city::LONDON:
+			os<<"런던";
+			return os;
+		case city::SEOUL:
+			os<<"서울";
+			return os;
+		case city::SEATTLE:
+			os<<"시애틀";
+			return os;
+		case city::DUBAI:
+			os<<"두바이";
+			return os;
+		case city::SYDNEY:
+			os<<"시드니";
+			return os;
+		default:
+			return os;
+	}
+}
+
+struct graph{
+	vector<vector<pair<int, int>>> data; 
+	
+	graph(int n){
+		//data는 n개의 vector를 가지고 있고, 각 vector는 pair<int, int>에 대한 원소를 가지고 있다
+		data = vector<vector<pair<int, int>>>(n, vector<pair<int, int>>());
+	}
+
+	void addEdge(const city c1, const city c2, int dis){
+		cout<<"Add Edge: "<<c1<<" - "<<c2<<" = "<<dis<<endl;
+
+		auto n1 = static_cast<int>(c1);
+		auto n2 = static_cast<int>(c2);
+
+		data[n1].push_back({n2, dis});
+		data[n2].push_back({n1, dis});
+	}
+
+	void removeEdge(const city c1, const city c2){
+		cout<<"Remove Edge: "<<c1<<" - "<<c2<<endl;
+
+
+		auto n1 = static_cast<int>(c1);
+		auto n2 = static_cast<int>(c2);
+
+		//[n2] 캡쳐
+		remove_if(data[n1].begin(), data[n1].end(), [n2](const auto& pair){
+				cout<<"if1. log "<<static_cast<city>(pair.first)<<endl;
+				return pair.first == n2;
+				});
+
+		remove_if(data[n2].begin(), data[n2].end(), [n1](const auto& pair){
+				cout<<"if2. log "<<static_cast<city>(pair.first)<<endl;
+				return pair.first == n1;
+				});
+		}
+};
+
+int main(){
+
+  graph g(6);
+
+  g.addEdge(city::LONDON, city::MOSCOW, 2500);
+  g.addEdge(city::LONDON, city::SEOUL, 9000);
+  g.addEdge(city::LONDON, city::DUBAI, 5500);
+  g.addEdge(city::SEOUL, city::MOSCOW, 6600);
+  g.addEdge(city::SEOUL, city::SEATTLE, 8000);
+  g.addEdge(city::SEOUL, city::DUBAI, 7000);
+  g.addEdge(city::SEOUL, city::SYDNEY, 8000);
+  g.addEdge(city::SEATTLE, city::MOSCOW, 8400);
+  g.addEdge(city::SEATTLE, city::SYDNEY, 12000);
+  g.addEdge(city::DUBAI, city::SYDNEY, 1200);
+
+  g.addEdge(city::SEATTLE, city::LONDON, 8000);
+  g.removeEdge(city::SEATTLE, city::LONDON);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+

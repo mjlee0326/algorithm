2c15879954d8406b4ee1b3b160800a143b54d2f8
mjlee0326 <mjlee032667@gmail.com>
2021-04-05 00:17:48 +0900

    unordered_STL

--- unordered_set_map.cpp
+++ unordered_set_map.cpp
@@ -12,11 +12,12 @@ void print(unordered_set<int>& container){
 
 void print(unordered_map<int, int>& container){
 	for(const auto& element : container)
-		cout<<element.first<<": "<<element.second<<", ";
+		cout<<element.first<<" -> "<<element.second<<", ";
 	cout<<endl;
 }
 
 void find(unordered_set<int>& container, const int element){
+	cout<<"search "<<element<<": ";
 	if(container.find(element) == container.end())
 		cout<<"not found "<<element<<endl;
 	else
@@ -24,6 +25,7 @@ void find(unordered_set<int>& container, const int element){
 }
 
 void find(unordered_map<int, int>& container, const int element){
+	cout<<"search "<<element<<": ";
 	auto it = container.find(element);
 	if(it == container.end())
 		cout<<"not found "<<element<<endl;
@@ -32,7 +34,9 @@ void find(unordered_map<int, int>& container, const int element){
 }
 
 int main(){
-	cout<<"unorderd_set"<<endl;
+	cout<<"------------------------------------"<<endl;
+	cout<<"unordered_set"<<endl;
+	cout<<"------------------------------------"<<endl;
 	unordered_set<int> set1 = {1,2,3,4,5};
 
 	cout<<"set1 init value: ";
@@ -55,6 +59,29 @@ int main(){
 	print(set1);
 
 	find(set1, 2);
+
+	cout<<"------------------------------------"<<endl;
+	cout<<"unoredered_map"<<endl;
+	cout<<"------------------------------------"<<endl;
+
+	unordered_map<int, int> map1; //square
+
+	map1.insert({2, 4});
+	map1[3] = 9;
+	cout<<"square of 2, 3: ";
+	print(map1);
+
+	map1[20] = 400;
+	map1[30] = 900;
+	cout<<"square of 20, 30: ";
+	print(map1);
+
+	find(map1, 10);
+	find(map1, 20);
+
+	cout<<"map1[30] = "<<map1[3]<<endl;
+	cout<<"map1[100] = "<<map1[100]<<endl;
+	print(map1);
 
 	return 0;
 }

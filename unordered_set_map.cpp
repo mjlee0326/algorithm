761ceb77d8c8a144ee4726b8773746192de163f7
mjlee0326 <mjlee032667@gmail.com>
2021-04-05 00:04:51 +0900

    unorderd

--- unordered_set_map.cpp
+++ unordered_set_map.cpp
@@ -10,22 +10,22 @@ void print(unordered_set<int>& container){
 	cout<<endl;
 }
 
-void print(unorederd_map<int, int>& container){
+void print(unordered_map<int, int>& container){
 	for(const auto& element : container)
 		cout<<element.first<<": "<<element.second<<", ";
 	cout<<endl;
 }
 
-void find(unorderd_set<int>& container, const int element){
+void find(unordered_set<int>& container, const int element){
 	if(container.find(element) == container.end())
 		cout<<"not found "<<element<<endl;
 	else
 		cout<<"Successfully found "<<element<<endl;
 }
 
-void find(unorderd_map<int>& container, const int element){
+void find(unordered_map<int, int>& container, const int element){
 	auto it = container.find(element);
-	if(it == coontainer.end())
+	if(it == container.end())
 		cout<<"not found "<<element<<endl;
 	else
 		cout<<"Successfully found  "<<it -> second<<endl;
@@ -33,22 +33,26 @@ void find(unorderd_map<int>& container, const int element){
 
 int main(){
 	cout<<"unorderd_set"<<endl;
-	unordert_set<int> set1 = {1,2,3,4,5};
+	unordered_set<int> set1 = {1,2,3,4,5};
 
-	cout<<"set1 init value: "<<print(set1);
+	cout<<"set1 init value: ";
+	print(set1);
 
 	set1.insert(2);
-	cout<<"insert 2: "<<print(set1);
+	cout<<"insert 2: ";
+	print(set1);
 
 	set1.insert(10);
 	set1.insert(300);
-	cout<<"insert 10, 300: "<< print(set1);
+	cout<<"insert 10, 300: ";
+	print(set1);
 
 	find(set1, 4);
 	find(set1, 100);
 
 	set1.erase(2);
-	cout<<"delete 2: "<<print(set1);
+	cout<<"delete 2: ";
+	print(set1);
 
 	find(set1, 2);
 

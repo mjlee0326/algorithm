d262e34e697ac65647fba7da3a56dd6a74b3576d
mjlee0326 <mjlee032667@gmail.com>
2021-04-07 23:27:35 +0900

    ing

---
+++ bestAlbum.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+
+		map<int> m;
+
+
+    return answer;
+}
+
+int main(){
+	vector<int> answer;
+
+	answer = solution({"classic", "pop", "classic", "classic", "pop"}, {500, 600, 150, 800, 255});
+	cout<<answer<<endl;
+}

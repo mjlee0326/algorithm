80f8b314a165b9ca32f197d81e2df367b9f8f92b
mjlee0326 <mjlee032667@gmail.com>
2021-05-02 22:46:41 +0900

    프로그래머스 멀쩡한 사각형

--- correct_square.cpp
+++ correct_square.cpp
@@ -2,19 +2,28 @@
 
 using namespace std;
 
-int gcb(int a, int b){
+int gcd(int a, int b){
 	if(a % b == 0)
 		return b;
-	return gcb(b, a%b);
+	return gcd(b, a%b);
 }
+
+// w와 h가 서로소인 사각형으로 축소 w' h'
+// 대각선은 도착지점에 도착할 때까지 w'-1만큼 평행이동, h'-1만큼 수직이동 -> 지날 때마다 사용할 수 없는 사각형 +1
+// 처음 지점(대각선 시작)의 정사각형 +1
+// 따라서 (w'-1) + (h'-1) + 1 = w' + h' - 1
+
+// 축소 시킨 사각형을 다시 복구하기 위해 공약수를 곱해줌
+
+// w * h - gcd (w, h)
 
 
 long long solution(int w,int h) {
     long long answer = (long long) w * (long long)h; 
-		int lcm;
+		int gcd_result;
 
-		lcm = gcb(w, h);
-		answer -= (w + h - lcm);
+		gcd_result = gcd(w, h);
+		answer -= (w + h - gcd_result);
 
 		return answer;
 }
